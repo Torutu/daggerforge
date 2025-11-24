fec8e47022d789e2d185bce14e2acbfbcc17c514
Waleed Alnaimi <waleedalnaimi117@gmail.com>
2025-11-25 01:10:17 +0200

    chore: barrels

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,21 +1,5 @@
-// Adversaries exports
-export * from './adversaries/components/AdvSearch';
-export * from './adversaries/components/AdvList';
-export * from './adversaries/components/AdvCounter';
-export * from './adversaries/creator/CardBuilder';
-export * from './adversaries/creator/FeatureManager';
-export * from './adversaries/creator/TextInputModal';
-export * from './adversaries/editor/AdvEditorModal';
-export * from './adversaries/editor/CardDataHelpers';
 
-// Environments exports
+export * from './adversaries/index';
 export * from './environments/index';
-
-// Encounters exports
 export * from './extra';
-
-// Dice exports
-export * from './extra';
-
-// Card Editor exports
 export * from './CardEditor';
